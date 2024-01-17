a88ddc46bf978333763196d750ca129e9d43c46d
Aria <ae24856@gmail.com>
2024-01-17 19:47:26 +0800

    Modify: forEach loop

--- all.js
+++ all.js
@@ -8,16 +8,9 @@ axios.get('./data.json')
 
 function renderData() {
     const content = document.querySelector(".content");
-
-    // 清空原有內容
-    content.innerHTML = '';
-
+    str = '';
     data.forEach(function (item, index) {
-        // 創建一個新的 div 元素
-        const newItem = document.createElement('div');
-
-        // 設定新元素的內容，使用模板字串
-        newItem.innerHTML = `
+        str += `
             <div class="item${index}">
                 <div class="category">
                     <img src="${item.icon}"" alt="">
@@ -27,9 +20,7 @@ function renderData() {
                 </div>
             </div>
         `;
-
-        // 將新元素加入 content 元素
-        content.appendChild(newItem);
+        content.innerHTML = str;
     });
 }
 
